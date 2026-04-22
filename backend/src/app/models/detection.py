5c044cb312e2234321e6a63ba10caeef54888d67
Ilya <titov.iiu@phystech.edu>
2026-04-22 23:38:50 +0300

    update model detection

---
+++ backend/src/app/models/detection.py
@@ -0,0 +1,30 @@
+from sqlalchemy import ForeignKey, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from .base import Base
+
+
+class ObjectType(Base):
+    __tablename__ = "object_type"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    detections: Mapped[List["Detection"]] = relationship(back_populates="object_type")
+
+
+class Detection(Base):
+    __tablename__ = "detection"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    image_id: Mapped[int] = mapped_column(ForeignKey("image.id"))
+
+    object_type_id: Mapped[int] = mapped_column(ForeignKey("object_type.id"))
+
+    y_min: Mapped[float] = mapped_column(Float, nullable=False)
+    y_max: Mapped[float] = mapped_column(Float, nullable=False)
+    x_min: Mapped[float] = mapped_column(Float, nullable=False)
+    x_max: Mapped[float] = mapped_column(Float, nullable=False)
+    confidenc: Mapped[float] = mapped_column(Float, nullable=False)
+
+    image: Mapped["Image"] = relationship(back_populates="detections")
+    object_type: Mapped["ObjectType"] = relationship(back_populates="detections")
